a836a32cc917446afc56e7509d48d9cfcc359291
bodya <solovey2048@gmail.com>
2019-03-13 23:24:17 +0200

    pre-aplpa build

--- view/src/main/java/ua/edu/sumdu/badgroup/App.java
+++ view/src/main/java/ua/edu/sumdu/badgroup/App.java
@@ -1,13 +1,24 @@
 package ua.edu.sumdu.badgroup;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.*;
+
+public class App extends Application
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("../../../../a.fxml"));
+        primaryStage.setTitle("Test");
+        primaryStage.setScene(new Scene(root, primaryStage.getWidth(), primaryStage.getHeight()));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
 }
