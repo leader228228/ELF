5fbcebf8fe6507e67d7a0173dc8e7ede7a4f134b
leader228228 <leader228228@gmail.com>
2019-03-13 19:29:36 +0200

    Initial commit

---
+++ src/main/java/ua/edu/sumdu/badgroup/App.java
@@ -0,0 +1,13 @@
+package ua.edu.sumdu.badgroup;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
